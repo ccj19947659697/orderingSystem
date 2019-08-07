2611e91cc3c23d6989e73126499682a0e72c3210
ccj19947659697 <48884557+ccj19947659697@users.noreply.github.com>
2019-08-07 18:57:04 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
 import VueRouter from 'vue-router'
-
 import Start from './components/Start.vue'
 import Home from './components/Home.vue'
 import Pcontent from './components/Pcontent.vue'
@@ -8,14 +7,14 @@ import Order from './components/Order.vue'
 import EditPeopleInfo from './components/EditPeopleInfo.vue'
 import Success from './components/Success.vue'
 const router = new VueRouter({
-  routes: [{path:'/',redirect:'/start'},
-           {path:'/start',component:Start},
-           {path:'/home',component:Home},
-           {path:'/pcontent',component:Pcontent},
-           {path:'/cart',component:Cart},
-           {path:'/order',component:Order},
-           {path:'/editpeopleinfo',component:EditPeopleInfo},
-           {path:'/success',component:Success}
+  routes: [{ path:'/',redirect:'/start' },
+           { path:'/start',component:Start },
+           { path:'/home',component:Home },
+           { path:'/pcontent',component:Pcontent },
+           { path:'/cart',component:Cart },
+           { path:'/order',component:Order },
+           { path:'/editpeopleinfo',component:EditPeopleInfo },
+           { path:'/success',component:Success }
            ]
 })
 
